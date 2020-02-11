c9cdcd258c553c3e9f05cc1228605e32479fb1aa
phil cowan <philipjcowan1@gmail.com>
2020-02-10 23:10:42 -0700

    starting the spotify function

--- liri.js
+++ liri.js
@@ -1,40 +1,55 @@
 require("dotenv").config();
-
+var Spotify = require("node-spotify-api")
 var keys = require("./keys.js");
-var axios =  require("axios");
+var axios = require("axios");
 var moment = require("moment");
 
-// var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 
-function concertThis(){
-var artist = process.argv.slice(3).join(" ");
-console.log(artist);
-var queryUrl = "https://rest.bandsintown.com/artists/"+ artist +"/events?app_id=codingbootcamp"
-axios.get(queryUrl).then(
-    function(response) {
-        var results = response.data
-        for (var i = 0; i < results.length; i++){
-            var venueName = results[i].venue.name;
-            var venueLoc = results[i].venue.city
-            var date = results[i].datetime
-            console.log(venueName+" "+venueLoc+" "+ moment(date).format("MM/DD/YYYY"))
-        }
+function concertThis() {
+    var artist = process.argv.slice(3).join(" ");
+    console.log(artist);
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    axios.get(queryUrl).then(
+        function (response) {
+            var results = response.data
+            for (var i = 0; i < results.length; i++) {
+                var venueName = results[i].venue.name;
+                var venueLoc = results[i].venue.city
+                var date = results[i].datetime
+                console.log(venueName + " " + venueLoc + " " + moment(date).format("MM/DD/YYYY"))
+            }
 
-    })
-}   
-function movieThis(){
+        })
+}
+function spotifyThis() {
+var spotify = new Spotify(keys.spotify);
+
+    var song = process.argv.slice(3).join(" ");
+    console.log(song);
+    spotify
+        .search({ type: 'track', query: song })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+
+function movieThis() {
     var movieName = process.argv.slice(3).join(" ");
-    if(process.argv.length < 4){
+    if (process.argv.length < 4) {
         movieName = "Mr Nobody"
-    }else{
+    } else {
         movieName = process.argv.slice(3).join(" ");
     }
     console.log(movieName);
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
     axios.get(queryUrl).then(
-        function(response) {
+        function (response) {
             var results = response.data
             console.log(results.Title)
             console.log(results.Year)
@@ -55,14 +70,12 @@ function movieThis(){
 switch (command) {
     case 'concert-this':
         concertThis();
-      break;
+        break;
     case 'spotify-this-song':
         spotifyThis();
         break;
     case 'movie-this':
         movieThis();
-      // expected output: "Mangoes and papayas are $2.79 a pound."
-      break;
+        break;
     default:
-      console.log('Sorry, we are out of ' + expr + '.');
-  }
+}
